47a69c16ebf5b318eed67b1127b78b7b492a21d9
Dilan <dilannebioglu@gmail.com>
2017-10-25 20:52:45 +0000

    seeding

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -8,3 +8,27 @@ exports.seed = function(knex, Promise) {
       ]);
     });
 };
+
+
+// exports.seed = function(knex, Promise) {
+  return Promise.all([
+    knex('users').del().then(function() {
+      knex('users').insert({name: 'Dilan'})
+    }),
+    knex('admins').del().then(function() {
+      knex('admins').insert({email: 'dilan@example.com'})
+    }),
+    knex('polls').del().then(function() {
+      knex('polls').insert()
+    }),
+  ])
+
+//   return knex('users').del()
+//     .then(function () {
+//       return Promise.all([
+//         knex('users').insert({id: 1, name: 'Alice'}),
+//         knex('users').insert({id: 2, name: 'Bob'}),
+//         knex('users').insert({id: 3, name: 'Charlie'})
+//       ]);
+//     });
+// };
