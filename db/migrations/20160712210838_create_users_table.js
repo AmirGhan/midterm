3b5a0cee2cba2cfa79990119bb04adbf8156e1fc
Dilan <dilannebioglu@gmail.com>
2017-10-25 19:56:40 +0000

    created table

--- db/migrations/20160712210838_create_users_table.js
+++ db/migrations/20160712210838_create_users_table.js
@@ -1,55 +1,28 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', function (table) {
-    table.increments();
-    table.string('name');
-  });
+  return Promise.all([
+    knex.schema.createTable('users', function(table){
+      table.increments('id').primary();
+    }),
+    knex.schema.createTable('admins', function(table) {
+      table.increments('id').primary();
+      table.string('email');
+    }),
+    knex.schema.createTable('polls', function(table) {
+      table.increments('id').primary();
+      table.integer('admin_id').references('id').inTable('admins');
+    }),
+    knex.schema.createTable('users_polls', function(table) {
+      table.integer('user_id').references('id').inTable('users');
+      table.integer('poll_id').references('id').inTable('polls');
+    }),
+  ])
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('users');
+  return Promise.all([
+    knex.schema.dropTable('users'),
+    knex.schema.dropTable('admins'),
+    knex.schema.dropTable('polls'),
+    knex.schema.dropTable('users_polls')
+  ])
 };
-
-
-
-
-
-// exports.up = function(knex, Promise) {
-//   return knex.schema.createTable('users', function (table) {
-//     table.increments('id').primary();
-//   });
-// };
-//
-// exports.down = function(knex, Promise) {
-//   return knex.schema.dropTable('users');
-// };
-
-// exports.up = function(knex, Promise) {
-//   return knex.schema.createTable('admins', function (table) {
-//     table.increments('id').primary();
-//     table.string('email');
-//   });
-// };
-//
-// exports.down = function(knex, Promise) {
-//   return knex.schema.dropTable('admins');
-// };
-
-// exports.up = function(knex, Promise) {
-//   return knex.schema.createTable('polls', function (table) {
-//     table.increments('id').primary();
-//     table.string('email');
-//   });
-// };
-//
-// exports.down = function(knex, Promise) {
-//   return knex.schema.dropTable('polls');
-// };
-
-// exports.up = function(knex, Promise) {
-//   return knex.schema.createTable('users_polls', function (table) {
-//   });
-// };
-//
-// exports.down = function(knex, Promise) {
-//   return knex.schema.dropTable('users_polls');
-// };
