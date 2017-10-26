4731bcb2d0f6870840bba6af8f05a66ad40b8166
Rileygowan <t.rileygowan@gmail.com>
2017-10-26 19:07:27 +0000

    starting transition to ejs

--- server/data-helpers.js
+++ server/data-helpers.js
@@ -6,9 +6,6 @@ module.exports = function makeDataHelpers(knex) {
   return {
   //id is sent by the ajax get request (loadPoll), and from routes as req.params.id, it is going to come here
   getPoll: function(pollId, callback) {
-<<<<<<< HEAD
-    knex('polls').join('options', 'polls.id', '=', 'options.poll_id').select('*').where('polls.id', '=', 'pollId')
-=======
 
     knex('polls')
     .join('options', 'polls.id', '=', 'options.poll_id')
@@ -20,7 +17,6 @@ module.exports = function makeDataHelpers(knex) {
       callback(err);
     });
   }
-    
->>>>>>> aaddbf9443f55a3e71e0e374320b11118cee2389
+
   }
 };
