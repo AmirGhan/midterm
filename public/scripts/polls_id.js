0f41bd3a782d941fbca715d9ea79d136e433571a
Rileygowan <t.rileygowan@gmail.com>
2017-10-26 15:01:41 +0000

    adding polls_id.js, polls_id.html, polls_id.css

---
+++ public/scripts/polls_id.js
@@ -0,0 +1,44 @@
+$(() => {
+
+
+//Sort options
+$( "#options" ).sortable();
+
+//Adding data attributes on submit
+$('.button').on('click', function(event){
+  $('.option').addClass('rank');
+})
+
+function createPoll(pollData) {
+  var $poll = $('#poll-template').clone();
+  $poll.removeAttr(hidden);
+  $poll.find('.poll-name').text(pollData['poll']['name'])
+  //How do you check for optional pollData? Does an if work?
+  // if (pollData['poll']['description']) {
+  //   $poll.find('.poll-desc).text(pollData['poll']['description']);
+  // }
+  appendOption(pollData)
+}
+
+function appendOptions(optionsArr) {
+  optionsArr.forEach(function(option){
+    //Also need to take optional poll fields into consideration
+    var newOption = createOptions(option);
+    $('#poll-template').append(newOption);
+  })
+}
+function createOptions(options) {
+  var $option = $('#poll-template .options').clone()
+  $option.removeAttr(hidden);
+  $option.find('.option-name').text(options['option']['name'])
+  //Also need to take optional poll fields into consideration
+  return $option;
+}
+
+
+
+
+
+
+
+});
