20f6e20928c3092e1482e7e2d7c558fba272cb4f
Rileygowan <t.rileygowan@gmail.com>
2017-10-26 18:04:22 +0000

    updating to allow pull

--- server/data-helpers.js
+++ server/data-helpers.js
@@ -6,5 +6,5 @@ module.exports = function makeDataHelpers(knex) {
   //id is sent by the ajax get request (loadPoll), and from routes as req.params.id, it is going to come here
   getPoll: function(pollId, callback) {
     knex('polls').join('options', 'polls.id', '=', 'options.poll_id').select('*').where('polls.id', '=', 'pollId')
-  })
+  }
 };
