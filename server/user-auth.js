e20d99b3179b3813b43619413a31ca69f050803f
Rileygowan <t.rileygowan@gmail.com>
2017-10-27 18:43:53 +0000

    userAuthHelpers

--- server/user-auth.js
+++ server/user-auth.js
@@ -1,45 +1,56 @@
 const bcrypt = require('bcrypt');
 
+const checkEmailUniqueness = function(email, knex) {
+    return new Promise((resolve, reject) => {
+      findByEmail(email, knex)
+      .then((user) => {
+      if (user) {
+        return reject({
+          type: 409,
+          message: 'email has already been used'
+        })
+      } else {
+        return resolve(email)
+      }
+    })
+  })
+}
+const findByEmail = function(email, knex) {
+      return new Promise((resolve, reject) => {
+        knex('admins')
+        .select('*')
+        .where({email: email})
+        .limit(1)
+        .then((rows) => {
+          user = rows[0]
+          return resolve(user)
+        })
+        .catch((error) => reject(error));
+      })
+    }
+
 module.exports = function makeAuthHelpers(knex) {
   return {
-    addUser: function(email, password) {
-      checkEmailUniqueness(email)
-      .then((email)=>{
-        return bcrypt.hash(password, 10);
+  addUser: function(email, password, callback) {
+    //syntax
+    return checkEmailUniqueness(email, knex)
+    .then((email)=>{
+      return bcrypt.hash(password, 10);
+    })
+    .then((passwordDigest)=>{
+      return knex('admins')
+      .returning('id')
+      .insert({
+        email: email,
+        password: passwordDigest
       })
-      .then((passwordDigest)=>{
-        return knex('admins').insert({
-          email: email,
-          password_digest: passwordDigest
-        })
+      .then((result) => {
+        console.log(result)
+        callback(null, result)
       })
-    },
-  checkEmailUniqueness: function(email) {
-    return new Promise((resolve, reject) => {
-      findByEmail(email)
-      .then((user) => {
-        if (user) {
-          return reject({
-            type: 409,
-            message: 'email has already been used'
-          })
-        } else {
-          return resolve(email)
-        }
-        })
+      .catch((err)=>{
+        callback(err)
       })
-    },
-    findByEmail: function(email) {
-    return new Promise((resolve, reject) => {
-      knex('admins')
-      .select('*')
-      .where({email: email})
-      .limit(1)
-      .then((rows) => {
-        user = rows[0]
-        return resolve(user)
-      })
-      .catch((error) => reject(error));
     })
   }
   }
