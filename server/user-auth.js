6a30f704d3708b3c194e978457c04165292ba162
Rileygowan <t.rileygowan@gmail.com>
2017-10-27 16:01:32 +0000

    knex insert USER data

---
+++ server/user-auth.js
@@ -0,0 +1,46 @@
+const bcrypt = require('bcrypt');
+
+module.exports = knex => ({
+  return {
+    addUser: function(email, password) {
+      checkEmailUniqueness(email)
+      .then((email)=>{
+        return bcrypt.hash(password, 10);
+      })
+      .then((passwordDigest)=>{
+        return knex('users').insert({
+          email: email,
+          password_digest: passwordDigest
+        })
+      })
+    }
+  checkEmailUniqueness: function(email) {
+    return new Promise((resolve, reject) => {
+      findByEmail(email)
+      .then((user) => {
+        if (user) {
+          return reject({
+            type: 409,
+            message: 'email has already been used'
+          })
+        } else {
+          return resolve(email)
+        }
+        })
+      })
+    }
+    findByEmail: function(email) {
+    return new Promise((resolve, reject) => {
+      knex('admins')
+      .select('*')
+      .where({email: email})
+      .limit(1)
+      .then((rows) => {
+        user = rows[0]
+        return resolve(user)
+      })
+      .catch((error) => reject(error));
+    })
+  }
+  }
+})
