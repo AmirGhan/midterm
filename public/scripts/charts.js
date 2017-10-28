d07ce90f7dc5433426219a5f8d73e09db0b67ab4
Dilan <dilannebioglu@gmail.com>
2017-10-28 21:34:23 +0000

    results chart

--- public/scripts/charts.js
+++ public/scripts/charts.js
@@ -4,28 +4,35 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
 
   var data = google.visualization.arrayToDataTable([
-    ['Option', 'Rank/Point'],
-    ['Lion King',     5],
-    ['Beauty and the Beast',      5],
-    ['Aladdin',  2]
+    ['Option', 'Rank'],
+    ['Option-Name', 'Rank-Points-Sum'],
+    ['Option-Name', 'Rank-Points-Sum'],
+    ['Option-Name', 'Rank-Points-Sum']
   ]);
 
   var options = {
-    title: 'Which Movie?'
+    title: 'POLL NAME'
   };
 
   var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
   chart.draw(data, options);
 }
+
 $(function(){
   console.log('connected');
   $('.poll-button').on('click', function() {
-    if ($('#piechart').attr('hidden')){
-      $('#piechart').removeAttr('hidden');
-    } else {
-      $('#piechart').attr('hidden', true);
-    }
-
+    $('#piechart').slideToggle();
   })
 })
+
+// option name, all the ranks and correspoding opt ids, pollname
+// poll.forEach(function(option) {
+//   let arr = [];
+//   let point = data-rank;
+//   let sum = sumPoint;
+//   let choiceId = data-optionId/optionId;
+//   arr.push('choiceId');
+//   arr.push(sum);
+//   return arr;
+// }
