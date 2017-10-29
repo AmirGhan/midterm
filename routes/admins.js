635de5bc0dccf6a4c33aea5e82707ae2c5dda20b
Dilan <dilannebioglu@gmail.com>
2017-10-29 00:19:58 +0000

    mailgun

--- routes/admins.js
+++ routes/admins.js
@@ -1,109 +1,116 @@
 "use strict";
 
 const express = require('express');
-const adminsRoutes  = express.Router();
+const adminsRoutes = express.Router();
 const userAuth = require('../server/user-auth.js');
 
+var api_key = '';
+var domain = '';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
 
 module.exports = function(dataHelpers) {
 
-// Register GET
+  // Register GET
 
-adminsRoutes.get('/register', function(req, res) {
-  res.render('register');
-})
+  adminsRoutes.get('/register', function(req, res) {
+    res.render('register');
+  })
 
-
-// Register POSTS\
+  // Register POSTS\
   adminsRoutes.post('/', function(req, res){
 
-    const {email, password} = req.body; // 1. user info captured, 2. save to databse by call insert admin
-    userAuth.addUser(email, password, function(err, adminId) {
-      if (err) {
-        let templateVars = {err: "Can not register"};
+  const {email, password} = req.body; // 1. user info captured, 2. save to databse by call insert admin
+  userAuth.addUser(email, password, function(err, adminId) {
+    if (err) {
+      let templateVars = {
+        err: "Can not register"
+      };
+      console.log(err);
+      res.render("register", templateVars);
+    } else {
+
+      res.status(201) //.send('User has been created');
+      res.redirect(`/admins/${adminId[0]}/polls`);
+    }
+  })
+
+}),
+
+adminsRoutes.post("/:id/polls/new", function(req, res) {
+  const options = req.body.option;
+  const pollName = req.body.pollName;
+  const admin_id = req.params.id;
+  const status = true;
+  dataHelpers.savePoll(pollName, admin_id, status, function(err, poll_id) {
+    options.forEach(function(option) {
+      console.log(option);
+      dataHelpers.saveOption(Number(poll_id), option, function(err, result) {
         console.log(err);
-        res.render("register", templateVars);
-      } else {
+        var data = {
+          from: 'Dilan <dilannebioglu@gmail.com>',
+          to: 'nebiogludilan@gmail.com',
+          subject: 'Hello from Decision Maker',
+          text: 'Congrats on creating a new poll! In case you missed the extremely important links to get you going from this point on, here they are: Your profile link & The link you want to send to your friends. Have a good day!'
+        };
 
-        res.status(201)//.send('User has been created');
-        res.redirect(`/admins/${adminId[0]}/polls`);
-      }
-    })
-
-  }),
-
-  adminsRoutes.post("/:id/polls/new", function(req, res){
-    const options = req.body.option;
-    const pollName = req.body.pollName;
-    const admin_id = req.params.id;
-    const status = true;
-    dataHelpers.savePoll(pollName, admin_id, status, function(err, poll_id){
-      options.forEach(function(option){
-        console.log(option);
-        dataHelpers.saveOption(Number(poll_id), option, function(err, result){
-          console.log(err);
-        })
+        mailgun.messages().send(data, function(error, body) {
+          console.log('email');
+          console.log(body);
+        });
       })
     })
-    res.redirect(`/admins/${admin_id}/polls`)
-  }),
-
-
+  })
+  res.redirect(`/admins/${admin_id}/polls`)
+}),
 
 //GETS
-  adminsRoutes.get("/:id/polls", function(req, res) {
-    const adminId = req.params.id;
-    dataHelpers.getAdminPolls(adminId, (err, polls) => {
+adminsRoutes.get("/:id/polls", function(req, res) {
+  const adminId = req.params.id;
+  dataHelpers.getAdminPolls(adminId, (err, polls) => {
     let templateVars = {
       polls: polls,
       adminId: adminId
     }
-      if (err) {
-        res.status(500).json({ error: err.message });
-      } else {
-        res.status(200).render('admin_polls', templateVars)
-      }
-    });
-  }),
-
-  adminsRoutes.get("/:id/polls/new", function(req, res){
-    const adminId = req.params.id;
-
     if (err) {
-      res.status(500).json({ error: err.message });
+      res.status(500).json({error: err.message});
     } else {
-      res.status(200).render('admin_new');
+      res.status(200).render('admin_polls', templateVars)
     }
-  }),
+  });
+}),
 
-  adminsRoutes.get("/:adminId/polls/:pollId", function(req, res) {
-    let adminId = req.params.adminId;
-    let pollId = req.params.pollId;
-    console.log("admin id: ", adminId, "poll id: ", pollId)
-    dataHelpers.getPollResult(adminId, pollId, (err, result) => {
-      console.log("*****MATH GOES HERE******", result)
+adminsRoutes.get("/:id/polls/new", function(req, res) {
+  const adminId = req.params.id;
 
-    })
-    res.send('hi')
+  if (err) {
+    res.status(500).json({error: err.message});
+  } else {
+    res.status(200).render('admin_new');
+  }
+}),
+
+adminsRoutes.get("/:adminId/polls/:pollId", function(req, res) {
+  let adminId = req.params.adminId;
+  let pollId = req.params.pollId;
+  console.log("admin id: ", adminId, "poll id: ", pollId)
+  dataHelpers.getPollResult(adminId, pollId, (err, result) => {
+    console.log("*****MATH GOES HERE******", result)
 
   })
-  // ,
-  //
-  // adminsRoutes.get("/:adminId/polls/:pollId", function(req, res) {
-  //   let adminId = req.params.adminId;
-  //   let pollId = req.params.pollId;
-  //   console.log("admin id: " adminId, "poll id: " pollId)
-  //   dataHelpers.getPollResult(adminId, pollId, (err, result) => {
-  //
-  //   })
-  //
-  // })
+  res.send('hi')
 
-
-
-
-
-
+})
+// ,
+//
+// adminsRoutes.get("/:adminId/polls/:pollId", function(req, res) {
+//   let adminId = req.params.adminId;
+//   let pollId = req.params.pollId;
+//   console.log("admin id: " adminId, "poll id: " pollId)
+//   dataHelpers.getPollResult(adminId, pollId, (err, result) => {
+//
+//   })
+//
+// })
 
 return adminsRoutes;
 }
