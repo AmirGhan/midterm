39b32b435bd7636d6caf7826b788491167dab1cc
Dilan <dilannebioglu@gmail.com>
2017-10-29 20:55:48 +0000

    completed charts show

--- public/scripts/charts.js
+++ public/scripts/charts.js
@@ -1,8 +1,8 @@
 $(function() {
-  $('#chart-button').on('click', function(event) {
+  $('.poll-button').on('click', function(event) {
     event.preventDefault();
     let $url = $(this).closest('#url').attr('name');
-    $('#piechart').slideToggle().removeAttr('hidden');
+    $('#piechartActive').toggle().removeAttr('hidden');
     $.ajax({
       url: $url,
       method: 'GET',
@@ -14,7 +14,6 @@ $(function() {
         for (let key in response) {
           arr.push([key, response[key]]);
         }
-        console.log(arr);
         google.charts.load('current', {'packages': ['corechart']});
         google.charts.setOnLoadCallback(drawChart);
 
@@ -22,7 +21,39 @@ $(function() {
 
           var data = google.visualization.arrayToDataTable(arr);
 
-          var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+          var chart = new google.visualization.PieChart(document.getElementById('piechartActive'));
+
+          chart.draw(data, arr);
+        }
+      }
+    })
+  })
+})
+
+$(function() {
+  $('.poll-button-closed').on('click', function(event) {
+    event.preventDefault();
+    let $url = $(this).closest('#urlClosed').attr('name');
+    $('#piechartClosed').toggle().removeAttr('hidden');
+    $.ajax({
+      url: $url,
+      method: 'GET',
+      dataType: 'json',
+      success: function(response) {
+        let arr = [
+          ['Option', 'Rank']
+        ];
+        for (let key in response) {
+          arr.push([key, response[key]]);
+        }
+        google.charts.load('current', {'packages': ['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+
+          var data = google.visualization.arrayToDataTable(arr);
+
+          var chart = new google.visualization.PieChart(document.getElementById('piechartClosed'));
 
           chart.draw(data, arr);
         }
