4b0651a97e46455db78072118c4bcd78e2c539c4
Rileygowan <t.rileygowan@gmail.com>
2017-10-30 13:59:35 +0000

    route changes

--- routes/polls.js
+++ routes/polls.js
@@ -4,7 +4,8 @@ const express = require('express');
 const pollsRoutes = express.Router();
 
 
-// var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+let mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+let mailcomposer = require('mailcomposer');
 
 module.exports = function(dataHelpers) {
 
@@ -35,41 +36,23 @@ module.exports = function(dataHelpers) {
     let pollObj = req.body;
     let pollOpt = pollObj.options;
     let link = "/polls/" + pollId;
-    var mail = mailcomposer({
-      from: 'Riley <t.rileygowan@gmail.com>',
-      to: 't.rileygowan@gmail.com',
+    let data = {
+      from: 'Dilan <dilannebioglu@gmail.com>',
+      to: 'nebiogludilan@gmail.com',
       subject: 'Hello from Decision Maker',
-      html: `Somebody voted on your poll! Check out the results here:<br><br><a href="http://localhost:8080/admins/${admin_id}/polls">Results</a><br><br>Have a nice day!`
-    });
-    mail.build(function(mailBuildError, message) {
-          var dataToSend = {
-            to: 't.rileygowan@gmail.com',
-            message: message.toString('ascii')
-          }
-
-    mailgun.messages().sendMime(dataToSend, function(sendError, body) {
-      if (sendError) {
-      console.log(sendError);
-      return;
-    }
-  })
-}
+      html: 'Somebody just voted! You can checkout the current results from your profile! <a href = link>Your profile link</a> Have a good day!'
+    };
 
     pollOpt.forEach(function(opt) {
       dataHelpers.addVotes(opt, function(err, result) {
-      if (err) {
-        res.status(500).json({error: err.message});
-        return;
-      }
-    })
-  res.status(200);
+        if (err) {
+          res.status(500).json({error: err.message});
+          return;
+        }
+      });
+    });
+    res.status(200);
   });
+
   return pollsRoutes;
-  };
-}
-
-
-
-
-
-
+};
